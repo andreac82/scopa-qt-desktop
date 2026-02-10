2c8723144362c322cb2d6e4fe0f32971dc3369ec
Andrea Callia D Iddio <a.calliadiddio@imperial.ac.uk>
2026-02-10 11:09:23 +0100

    Start the app maximised to prevent a bug about inconsistend card sizes.

--- qt-application/main.cpp
+++ qt-application/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     
     MainWindow mainWindow(&app);
-    mainWindow.show();
+    mainWindow.showMaximized();
 
     return app.exec();
 }
